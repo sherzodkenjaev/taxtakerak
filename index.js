a0f9891057534c55446be66b57bf454b84dbd0d5
sherzodkenjaev <81644562+sherzodkenjaev@users.noreply.github.com>
2021-07-31 22:38:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('button').onclick = function changeContent() {
+  var uyEni = document.getElementById("uyEni").value;
+  var uyBoyi = document.getElementById("uyBoyi").value;
+
+  var taxtaEni = document.getElementById("taxtaEni").value;
+  var taxtaBoyi = document.getElementById("taxtaBoyi").value;
+
+  var result = (uyEni*uyBoyi)/(taxtaEni*taxtaBoyi);
+
+  return document.getElementById("label").innerHTML = result.toPrecision(4);
+  }
